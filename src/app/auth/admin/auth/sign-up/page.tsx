fea2b11bd9c72e441f13447e2f6ad5fc7ff7aa58
delegram-pixel <bamideleaug2002@gmail.com>
2024-10-24 09:59:21 +0100

    done

--- src/app/auth/admin/auth/sign-up/page.tsx
+++ src/app/auth/admin/auth/sign-up/page.tsx
@@ -10,7 +10,7 @@ type Props = {};
 const SignUp = (props: Props) => {
   return (
     <div className="flex-1 py-36 md:px-16 w-full">
-      {/* <div className="flex flex-col h-full gap-3">
+      <div className="flex flex-col h-full gap-3">
         <SignUpFormProvider>
           <div className="flex flex-col gap-3 ">
             <RegistrationFormStep />
@@ -18,7 +18,7 @@ const SignUp = (props: Props) => {
             <HighLightBar/>
           </div>
         </SignUpFormProvider>
-      </div> */}
+      </div>
     </div>
   );
 };
