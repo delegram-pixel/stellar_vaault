65a971c5165cdc90615ef66b1a7c62e530c3b593
delegram-pixel <bamideleaug2002@gmail.com>
2024-10-26 09:26:07 +0100

    done

---
+++ src/components/site/JivoChat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+const JivoChat: React.FC = () => {
+  return (
+    <div>
+     <script src="//code.jivosite.com/widget/" async></script>
+
+    </div>
+  );
+};
+
+export default JivoChat;
